dd0a7a86544c19670a8a52bbbbec218a4bda954c
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2025-01-19 20:20:11 +0530

    created recursive input reads

--- app/main.ts
+++ app/main.ts
@@ -9,9 +9,17 @@ const rl = createInterface({
 const commands = ["cd", "ls", "pwd", "exit"];
 
 // Uncomment this block to pass the first stage
-rl.question("$ ", (answer: string) => {
+const question = () => {rl.question("$ ", (answer: string) => {
+
+  if(answer === "exit"){
+    rl.close();
+  }
+
   if (!commands.includes(answer)) {
     console.log(`${answer}: command not found`);
+    question(); 
   }
-  rl.close();
-});
+  
+})};
+
+question(); 
