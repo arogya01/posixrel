97d57361697e42d587480de79725598c8457231c
Arogya Bichpuria <arogya.bichpuria@M-FVFHN7U3Q05N.local>
2025-02-08 19:54:43 +0530

    added type builtin

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,7 @@ const rl = createInterface({
 });
 
 const commands = ["cd", "ls", "pwd", "echo"];
-
+const validCommands = ["type", "echo", "exit"];
 // Uncomment this block to pass the first stage
 const question = () => {
   rl.question("$ ", (answer: string) => {
@@ -14,7 +14,12 @@ const question = () => {
       process.exit(0);
     }
 
-    if (answer.startsWith("echo")) {
+    if (answer.startsWith("type")) {
+      const output = answer.split(" ").slice(1).join(" ") || "";
+      if (validCommands.includes(output)) {
+        console.log(`${output} is a shell builtin`);
+      }
+    } else if (answer.startsWith("echo")) {
       const output = answer.split(" ").slice(1).join(" ") || "";
       console.log(output);
     } else if (!commands.includes(answer)) {
