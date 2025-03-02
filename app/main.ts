f45e5bef365e99b2ca8e30cea06e6329e156a546
arogya <arogya.bichpuria16@gmail.com>
2025-03-02 22:36:26 +0530

    ability to run custom commands

--- app/main.ts
+++ app/main.ts
@@ -1,27 +1,42 @@
 import { createInterface } from "readline";
 import fs from "fs"; 
+import { execSync } from "child_process"; 
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+
+// Tests
+// The tester will execute your program like this:
+
+// ./your_program.sh
+// It'll then send a command that you need to execute:
+
+// $ custom_exe_1234 alice
+// Program was passed 2 args (including program name).
+// Arg #0 (program name): custom_exe_1234
+// Arg #1: alice
+// Program Signature: 5998595441
+
 const commands = ["cd", "ls", "pwd", "echo"];
 const validCommands = ["type", "echo", "exit"];
-// Uncomment this block to pass the first stage
+const paths = (process.env.PATH as string).split(":") ?? [];
+
 const question = () => {
   rl.question("$ ", (answer: string) => {
     if (answer === "exit 0") {
       process.exit(0);
     }
 
-    if (answer.startsWith("type")) {
-      const output = answer.split(" ").slice(1).join(" ") || "";
+    const [cmd, ...args] = answer.split(" "); 
+    if (cmd === "type") {
+      const output = args.join(" ") || "";
       if (validCommands.includes(output)) {
         console.log(`${output} is a shell builtin`);
       } else {
         let found = false; 
-        const paths = process.env.PATH?.split(":") ?? []; 
         paths?.forEach((path)=> {
           try{
             const cmds = fs.readdirSync(path).filter((cmd) => cmd === output); 
@@ -39,13 +54,28 @@ const question = () => {
           console.log(`${output}: not found`);
         }
       }
-    } else if (answer.startsWith("echo")) {
-      const output = answer.split(" ").slice(1).join(" ") || "";
+    } else if (cmd === "echo") {
+      const output = args.join(" ") || "";
       console.log(output);
-    } else if (!commands.includes(answer)) {
-      console.log(`${answer}: command not found`);
+    } else if (!commands.includes(cmd)) {
+      let found = false; 
+      for(const path of paths){
+        try{
+          const foundCommand = fs.readdirSync(path).find((file) => file === cmd);
+          if(foundCommand){
+            found = true; 
+            const result = execSync(`${path}/${foundCommand} ${args.join(" ")}`); 
+            console.log(result.toString()); 
+            break;
+          }
+        }
+        catch(error){          
+        }
+      }
+      if(!found){
+        console.log(`${cmd}: command not found`);
+      }
     }
-
     question();
   });
 };
